65333d8db9808d4be0481d1e79cdd2c02214cb1d
Anes Cehic <anes.cehic@gmail.com>
2022-01-29 12:45:17 +0100

    Initial navigation setup

---
+++ src/navigation/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import LoginScreen from './LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainNavigation;
