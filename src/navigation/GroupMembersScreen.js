6fd0b3a44ef8992dba9a1cbbd22c62e672460cf5
Anes Cehic <anes.cehic@gmail.com>
2022-03-23 20:38:45 +0100

    Groups screens added header background color

--- src/navigation/GroupMembersScreen.js
+++ src/navigation/GroupMembersScreen.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {GroupMembers} from '@containers';
+import {Styles} from '@common';
 
 const GroupMembersScreen = ({navigation, route}) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: Styles.Colors.gold,
+      },
+    });
+  }, []);
   return <GroupMembers navigation={navigation} route={route} />;
 };
 
--- src/navigation/GroupsScreen.js
+++ src/navigation/GroupsScreen.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {Groups} from '@containers';
+import {Styles} from '@common';
 
 const GroupsScreen = ({navigation}) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: Styles.Colors.gold,
+      },
+    });
+  }, []);
+
   return <Groups navigation={navigation} />;
 };
 
