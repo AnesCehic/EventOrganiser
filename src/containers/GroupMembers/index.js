e9e017fe71c1d00481d1f5198dbdde8698d23a13
Anes Cehic <anes.cehic@gmail.com>
2022-04-06 00:33:00 +0200

    Group members screen UI update

--- src/containers/GroupMembers/index.js
+++ src/containers/GroupMembers/index.js
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, View, Text, Image, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-remix-icon';
+import IconFeathers from 'react-native-vector-icons/Feather';
+import IconMAC from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Avatar} from 'react-native-elements';
 
-import {LoadingIndicator, MenuItem} from '@components';
+import {LoadingIndicator, HeaderBack} from '@components';
 import {GroupService, UsersService} from '@services/apiClient';
 import {toast} from '@utils';
+import {Styles} from '@common';
 
 import styles from '../Groups/styles';
+import userCardStyle from './styles';
 
 const GroupMembers = ({navigation, route}) => {
   const [groupMembers, setGroupMembers] = useState([]);
@@ -41,8 +47,28 @@ const GroupMembers = ({navigation, route}) => {
 
   const renderGroupMember = ({item: member}) => {
     return (
-      <View>
-        <MenuItem
+      <View style={userCardStyle.container}>
+        <View style={userCardStyle.userInfo}>
+          <Avatar
+            source={{
+              uri: 'https://api.uifaces.co/our-content/donated/KtCFjlD4.jpg',
+            }}
+            rounded
+            size={48}
+          />
+          <View style={{marginLeft: 14}}>
+            <Text style={userCardStyle.name}>{member.text}</Text>
+            <Text style={userCardStyle.email}>{member.text}</Text>
+          </View>
+        </View>
+
+        <Icon
+          name="ri-arrow-right-s-line"
+          size={30}
+          color={Styles.Colors.grayText}
+          // style={styles.arrowIcon}
+        />
+        {/* <MenuItem
           key={member._id}
           groupId={member._id}
           onPress={() => {
@@ -51,7 +77,7 @@ const GroupMembers = ({navigation, route}) => {
             });
           }}
           menuText={`${member.firstName} ${member.lastName}`}
-        />
+        /> */}
       </View>
     );
   };
@@ -63,14 +89,68 @@ const GroupMembers = ({navigation, route}) => {
     });
   }, []);
 
+  const renderHeader = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: Styles.Colors.gold,
+          paddingTop: 48,
+          paddingHorizontal: 16,
+          paddingBottom: 20,
+          marginBottom: 20,
+        }}>
+        <View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <HeaderBack onPress={() => navigation.goBack()} />
+            <TouchableOpacity
+              onPress={() => null}
+              style={{
+                backgroundColor: Styles.Colors.white,
+                borderRadius: 50,
+                height: 35,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingHorizontal: 16,
+              }}>
+              <IconMAC
+                name="checkbox-marked-circle-outline"
+                size={16}
+                style={{marginRight: 9}}
+              />
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: '700',
+                }}>
+                Joined
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{marginTop: 16}}>
+            <Text style={{fontSize: 28, fontWeight: '700'}}>Lincoln 2022</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <IconFeathers name="user" size={13} style={{marginRight: 6}} />
+              <Text style={{fontSize: 13, fontWeight: '400'}}>71,000</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   if (isLoading) {
     return <LoadingIndicator />;
   }
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, userCardStyle.paddingOverride]}>
       <FlatList
-        data={groupMembers}
+        ListHeaderComponent={renderHeader}
+        data={[
+          {id: 1, text: '123'},
+          {id: 2, text: '345'},
+        ]}
         keyExtractor={item => item._id}
         renderItem={renderGroupMember}
         refreshing={refreshing}
---
+++ src/containers/GroupMembers/styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from 'react-native';
+import {Styles} from '@common';
+
+const userCardStyle = StyleSheet.create({
+  container: {
+    elevation: 5,
+    borderColor: Styles.Colors.gray,
+    backgroundColor: Styles.Colors.white,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 16,
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginBottom: 16,
+    width: '90%',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  paddingOverride: {
+    padding: 0,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  email: {
+    fontSize: 12,
+    fontWeight: '400',
+  },
+  listHeader: {
+    width: '90%',
+    textAlign: 'left',
+    alignSelf: 'center',
+    marginBottom: 12,
+    marginTop: 16,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+export default userCardStyle;
