dc9ce908f277abd776362ede08a87c88ce431823
Anes Cehic <anes.cehic@gmail.com>
2022-04-26 00:23:09 +0200

    Custom back button on post screen

--- src/navigation/PostsDetailsScreen.js
+++ src/navigation/PostsDetailsScreen.js
@@ -3,12 +3,16 @@ import {Text} from 'react-native';
 
 import {PostDetails} from '@containers';
 import {Styles} from '@common';
+import {HeaderBack} from '@components';
 
 const PostDetailsSCreen = ({navigation, route}) => {
   useEffect(() => {
     navigation.setOptions({
       headerTitleAlign: 'center',
       headerTintColor: Styles.Colors.white,
+      headerLeft: () => {
+        return <HeaderBack onPress={() => navigation.goBack()} />;
+      },
     });
   }, []);
 
