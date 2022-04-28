c51001bf9ce6ebaf2d3157914c1efc50a9c302b6
Anes Cehic <anes.cehic@gmail.com>
2022-04-29 01:59:18 +0200

    Comment preview fix

--- src/containers/Comments/Comment.js
+++ src/containers/Comments/Comment.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import dayjs from 'dayjs';
 
 import styles from './styles';
 
 const Comment = ({post}) => {
+  console.log('comment', post)
   return (
     <View style={styles.commentContainer}>
       {/* {!post?.owner?.uploadId ? ( */}
       <View>
-        <Text style={styles.userImageFallback}>
-          {`${post?.owner?.firstName[0]}${post?.owner?.lastName[0]}`}
-        </Text>
+        {post?.owner?.upload?.files[0].signedURL ? (
+          <Image
+            source={{
+              uri: post?.owner?.upload?.files[0].signedURL,
+            }}
+            resizeMode="cover"
+            style={{
+              width: 32,
+              height: 32,
+              borderRadius: 50,
+              marginHorizontal: 8,
+              marginTop: 11,
+            }}
+          />
+        ) : (
+          <Text style={styles.userImageFallback}>
+            {`${post?.owner?.firstName[0]}${post?.owner?.lastName[0]}`}
+          </Text>
+        )}
       </View>
       {/* ) : null} */}
-      <View style={{flexGrow: 1}}>
+      <View style={{flexGrow: 1, flexShrink: 1}}>
         <View style={styles.userNameAndDate}>
           <Text
             style={
