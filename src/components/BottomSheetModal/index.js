52a15c94e3ad57ac7e36ec202347321766b71158
Nickie <ninoslavbb@gmail.com>
2022-05-04 17:56:35 +0200

    my events pagination, keyboard avoid view on bottom sheet modal (#64)
    fixed dropdown item

--- src/components/BottomSheetModal/index.js
+++ src/components/BottomSheetModal/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
 import Modal from 'react-native-modal';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 import styles from './styles';
 
@@ -21,15 +22,17 @@ const BottomSheetModal = ({
       style={[styles.container, containerStyle]}
       backdropColor="#0000001c"
       onBackdropPress={closeModal}>
-      <View
-        style={[
+      <KeyboardAwareScrollView
+        enableOnAndroid
+        extraScrollHeight={70}
+        contentContainerStyle={[
           styles.contentContainer,
           {paddingBottom: insets.bottom},
           contentContainerStyle,
         ]}>
         {title ? <Text style={styles.title}>{title}</Text> : null}
         {children}
-      </View>
+      </KeyboardAwareScrollView>
     </Modal>
   );
 };
