5ce0af2344b1aa6f50326f04a79b315349e6a136
Anes Cehic <anes.cehic@gmail.com>
2022-05-07 01:11:00 +0200

    Added background video load fallback image

--- src/containers/Start/index.js
+++ src/containers/Start/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, Image, View, ImageBackground, Appearance} from 'react-native';
 import Video from 'react-native-video';
 
@@ -7,6 +7,7 @@ import {SubmitButton, BottomStartScreenButton} from '@components';
 import styles from './styles';
 
 const Start = ({navigation}) => {
+  const [videoLoading, setVideoLoading] = useState(true);
   const colorScheme = Appearance.getColorScheme();
 
   const render = () => {
@@ -20,72 +21,81 @@ const Start = ({navigation}) => {
     );
   };
 
-  const renderChildren = () => {
-    return (
-      <>
-        <View
+  const renderChildren = () => (
+    <>
+      <View
+        style={{
+          flex: 1,
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          opacity: 0.7,
+          backgroundColor: 'white',
+        }}>
+        <Video
+          source={{
+            uri: 'https://storage.googleapis.com/lincoln-club-community-app-assets/login-video-background.mp4',
+          }}
+          onLoad={() => setVideoLoading(false)}
           style={{
-            flex: 1,
+            height: '100%',
+            width: '100%',
             position: 'absolute',
             top: 0,
             right: 0,
             bottom: 0,
             left: 0,
-            opacity: 0.7,
-            backgroundColor: 'white',
-          }}>
-          <Video
-            source={{
-              uri: 'https://storage.googleapis.com/lincoln-club-community-app-assets/login-video-background.mp4',
-            }}
-            style={{
-              height: '100%',
-              width: '100%',
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              opacity: 0.5,
-            }}
-            muted={true}
-            repeat={true}
-            rate={1.0}
-            resizeMode="cover"
-            ignoreSilentSwitch={'obey'}
-          />
-        </View>
-        <View style={styles.overlay} />
-        <Image
-          source={require('../../assets/Home/white.png')}
-          style={styles.lincolnImage}
+            opacity: 0.5,
+          }}
+          muted={true}
+          repeat={true}
+          rate={1.0}
+          resizeMode="cover"
+          ignoreSilentSwitch={'obey'}
         />
-        <View style={styles.headerContainer}>
-          <Text style={styles.headers}>Welcome to{'\n'}Lincoln Club</Text>
+      </View>
+      <View style={[styles.overlay, {opacity: videoLoading ? 1 : 0.5}]} />
+      {videoLoading ? (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Image source={require('../../assets/Home/white.png')} />
         </View>
-        <View style={styles.container}>
-          <SubmitButton
-            onPress={() => navigation.navigate('Login')}
-            title="Sign in with Google"
-            style={styles.googleButton}
-            googleLogo
-            titleStyle={styles.googleTextStyle}
+      ) : (
+        <>
+          <Image
+            source={require('../../assets/Home/white.png')}
+            style={styles.lincolnImage}
           />
-          <SubmitButton
-            onPress={() => navigation.navigate('Login')}
-            title="Sign in with email"
-            style={styles.button}
-            titleStyle={styles.textStyle}
-          />
-        </View>
+          <View style={styles.headerContainer}>
+            <Text style={styles.headers}>Welcome to{'\n'}Lincoln Club</Text>
+          </View>
+          <View style={styles.container}>
+            <SubmitButton
+              onPress={() => navigation.navigate('Login')}
+              title="Sign in with Google"
+              style={styles.googleButton}
+              googleLogo
+              titleStyle={styles.googleTextStyle}
+            />
+            <SubmitButton
+              onPress={() => navigation.navigate('Login')}
+              title="Sign in with email"
+              style={styles.button}
+              titleStyle={styles.textStyle}
+            />
+          </View>
+        </>
+      )}
 
+      {videoLoading ? null : (
         <BottomStartScreenButton
           onPress={() => navigation.navigate('Register')}
           text="Sign up for an account"
         />
-      </>
-    );
-  };
+      )}
+    </>
+  );
   return <>{render()}</>;
 };
 
