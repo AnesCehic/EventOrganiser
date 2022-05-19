ee0ca366f3fda05d115861fe69cd129df7641f90
Anes Cehic <anes.cehic@gmail.com>
2022-05-19 19:42:16 +0200

    Notification IOS basic config

--- index.js
+++ index.js
@@ -11,49 +11,16 @@ import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
 
 PushNotification.configure({
-  onRegister: function (token) {
-    console.log('TOKEN:', token);
-  },
-
-  // (required) Called when a remote is received or opened, or local notification is opened
-  onNotification: function (notification) {
-    console.log('NOTIFICATION:', notification);
-
-    // process the notification
-
-    // (required) Called when a remote is received or opened, or local notification is opened
-    // notification.finish(PushNotificationIOS.FetchResult.NoData);
-  },
-
-  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-  onAction: function (notification) {
-    console.log('ACTION:', notification.action);
-    console.log('NOTIFICATION:', notification);
-  },
-
-  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-  onRegistrationError: function (err) {
-    console.error(err.message, err);
-  },
-
-  // IOS ONLY (optional): default: all - Permissions to register.
+  onRegister: function (token) {},
+  onNotification: function (notification) {},
+  onAction: function (notification) {},
+  onRegistrationError: function (err) {},
   permissions: {
     alert: true,
     badge: true,
     sound: true,
   },
-
-  // Should the initial notification be popped automatically
-  // default: true
   popInitialNotification: true,
-
-  /**
-   * (optional) default: true
-   * - Specified if permissions (ios) and token (android and ios) will requested or not,
-   * - if not, you must call PushNotificationsHandler.requestPermissions() later
-   * - if you are not using remote notification or do not have Firebase installed, use this:
-   *     requestPermissions: Platform.OS === 'ios'
-   */
   requestPermissions: Platform.OS === 'ios',
 });
 
@@ -65,9 +32,7 @@ PushNotification.createChannel(
     importance: 4, // (optional) default: 4. Int value of the Android notification importance
     vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
   },
-  created => {
-    console.log('channel created');
-  }, // (optional) callback returns whether the channel was created, false means it already existed.
+  created => {}, // (optional) callback returns whether the channel was created, false means it already existed.
 );
 
 AppRegistry.registerComponent(appName, () => App);
