f9df6c3cd8c70198354b3d9ce58cd4ae3b90eecb
Anes Cehic <anes.cehic@gmail.com>
2022-05-21 22:37:06 +0200

    Message sender wont receive notification

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,19 +1,34 @@
+import AsyncStorageLib from '@react-native-async-storage/async-storage';
 import PushNotification from 'react-native-push-notification';
 import PushNotificationIos from '@react-native-community/push-notification-ios';
 
-const presentNotification = message => {
-  PushNotification.localNotification({
-    channelId: 'specialid',
-    title: `Someone sent you a message.`,
-    message: message.text,
-  });
+const presentNotification = async message => {
+  try {
+    const userId = await AsyncStorageLib.getItem('@userId');
+    if (userId !== message.ownerId) {
+      PushNotification.localNotification({
+        channelId: 'specialid',
+        title: 'Someone sent you a message.',
+        message: message.text,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const presentNotificationIos = message => {
-  PushNotificationIos.addNotificationRequest({
-    title: 'Someone sent you a message',
-    body: message.text,
-  });
+const presentNotificationIos = async message => {
+  try {
+    const userId = await AsyncStorageLib.getItem('@userId');
+    if (userId !== message.ownerId) {
+      PushNotificationIos.presentLocalNotification({
+        alertTitle: 'Someone sent you a message',
+        alertBody: message.text,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export {presentNotification, presentNotificationIos};
