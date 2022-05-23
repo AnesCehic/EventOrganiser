d9dcc81fd5f8bdcf75ee82c261ef0b3a71debaa8
Anes Cehic <anes.cehic@gmail.com>
2022-05-24 01:22:31 +0200

    Loading screen dark mode

--- src/components/LoadingIndicator/index.js
+++ src/components/LoadingIndicator/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {View, ActivityIndicator, StyleSheet} from 'react-native';
+import {
+  View,
+  ActivityIndicator,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+import {Styles} from '@common';
 
 const LoadingIndicator = () => {
+  const colorScheme = useColorScheme();
   return (
-    <View style={styles.container}>
-      <ActivityIndicator size="large" color="black" />
+    <View
+      style={[
+        styles.container,
+        colorScheme === 'dark' && styles.containerDarkBackground,
+      ]}>
+      <ActivityIndicator
+        size="large"
+        color={
+          colorScheme === 'dark' ? Styles.Colors.white : Styles.Colors.black
+        }
+      />
     </View>
   );
 };
@@ -14,7 +31,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: Styles.Colors.white,
+  },
+  containerDarkBackground: {
+    backgroundColor: '#0A121A',
   },
 });
 
